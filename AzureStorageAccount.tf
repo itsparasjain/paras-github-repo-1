6ba384bd2eeda8c0042348a57a180f646b7bc291
itsparasjain <135979900+itsparasjain@users.noreply.github.com>
2023-09-25 11:34:47 +0000

    Update AzureStorageAccount.tf

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "demo" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = "rg-github-sea-001"
+  location = "southeastasia"
 }
 
 ##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "satestant000012"
+  name                     = "storageaccount10080"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
